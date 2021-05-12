ccc29be16d1f3602083f932b4fd5044f99a96ef4
MateuszWys <72884715+MateuszWys@users.noreply.github.com>
2021-05-12 18:22:52 +0200

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,8 +1,14 @@
+const process = (data) =>{
+  for (let i = 0; i < data.length; i += 4) {
+    const [r, g, b] = [data[i], data[i + 1], data[i + 2]];
 
-import * as filters from "./filters.js";
-
-self.onmessage = e => {
-  const { imageData, filter } = e.data;
-  filters[filter](imageData);
-  self.postMessage(imageData, [imageData.data.buffer]);
+    // CIE luminance for the RGB
+    // The human eye is bad at seeing red and blue, so we de-emphasize them.
+    data[i] = data[i + 1] = data[i + 2] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  }
 };
+
+addEventListener('message', ({ data }) => {
+  process(data.imageData.data);
+  postMessage(data.imageData);
+});
